dd41b1ae526d14e1cbbeffb4964e7af12fe7a51c
Yunchen Zhu <yunchez3@uci.edu>
2021-06-11 13:24:29 -0700

    first commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions
+          .map((tx) => Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.deepOrange, width: 2),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Text('\$' + tx.amount.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.purple))),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black),
+                        ),
+                        Text(DateFormat().format(tx.date),
+                            style: TextStyle(color: Colors.grey, fontSize: 14)),
+                      ],
+                    )
+                  ],
+                ),
+              ))
+          .toList(),
+    );
+  }
+}
