fdece6f7c982c436ca680da6b07d8676ab575952
Yunchen Zhu <yunchez3@uci.edu>
2021-06-11 13:38:26 -0700

    scollable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,42 +10,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map((tx) => Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Colors.deepOrange, width: 2),
-                            borderRadius: BorderRadius.circular(12)),
-                        child: Text('\$' + tx.amount.toString(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.purple))),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 700,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions
+              .map((tx) => Card(
+                    child: Row(
                       children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.black),
-                        ),
-                        Text(DateFormat().format(tx.date),
-                            style: TextStyle(color: Colors.grey, fontSize: 14)),
+                        Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 50),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.deepOrange, width: 2),
+                                borderRadius: BorderRadius.circular(12)),
+                            child: Text('\$' + tx.amount.toString(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.purple))),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.black),
+                            ),
+                            Text(DateFormat().format(tx.date),
+                                style: TextStyle(
+                                    color: Colors.grey, fontSize: 14)),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ))
-          .toList(),
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
